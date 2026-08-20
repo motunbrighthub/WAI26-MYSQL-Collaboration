e6535ca33a8320b1d70e7d0e5c5a7041805c4f0f
Carina <carinaigbodor0@gmail.com>
2026-08-20 11:52:53 -0700

    Question 15, 16

---
+++ Question 15,16.sql
@@ -0,0 +1,17 @@
+-- Subqueries 15. A company wants to identify countries whose population is greater than the average population of all countries. 
+-- Hint: Compare each country's population with AVG(Population).
+
+Select
+name,
+population
+from country
+where population > ( Select avg(population) as avg_pop
+					from country);
+				
+-- 16. An investment analyst wants to identify countries whose GNP is greater than the average GNP of all countries
+Select
+Name,
+GNP
+from country
+where GNP > (Select AVG(GNP) as avg_gnp
+				from country);
